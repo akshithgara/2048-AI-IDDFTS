8b1e13fb9c3022a75d94fe30c92a95b592387192
markmyersii <markmyers2@me.com>
2020-02-17 18:39:09 -0600

    Initial Repo

---
+++ run.sh
@@ -0,0 +1,10 @@
+#~\bin\bahs
+
+# You will put your execution script here
+# below is an example of a compiled and executed C++ program
+# you may modify this script or delete it and start from scratch
+# your bash script must be named "run.sh"
+
+g++ -std=c++11 *.cpp -o HelloAI
+./HelloAI
+
